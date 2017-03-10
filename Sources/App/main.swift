a06cf717d1cff49e9e736778d8159a1742c01a42
Gabriel Roupillard <gabriel@dyna.mo>
2017-03-10 02:19:36 +0100

    $1

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -10,10 +10,33 @@ drop.get("hello/info") { request in
     return "This is the page where we could show more info"
 }
 
+// ROOT
 drop.get { req in
     return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
+    	"message": drop.localization[req.lang, "welcome", "Dynamo-server"]
     ])
+}
+
+// ANIMATION
+drop.get("animation") { request in
+    return try drop.view.make("dynamo.html")
+}
+drop.get("dynamo_hype_generated_script.js") { request in
+    return try drop.view.make("dynamo/dynamo_hype_generated_script.js")
+}
+drop.get("HYPE-576.thin.min.js") { request in
+    return try drop.view.make("dynamo/HYPE-576.thin.min.js")
+}
+drop.get("dynamo_logo.png") { request in
+    return try drop.view.make("dynamo/dynamo_logo.png")
+}
+drop.get("PIE.htc") { request in
+    return try drop.view.make("dynamo/PIE.htc")
+}
+
+// DICTIONARY
+drop.get("hello/info") { request in
+    return "This is the page where we could show more info"
 }
 
 drop.resource("posts", PostController())
